1d0b86f9076f64a658ba7a71fbf5d0e7e2f98af8
trevdogk2 <trevorwwestin@gmail.com>
2024-02-25 17:15:05 -0500

    Video 1 pub

--- colorGrabber.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import time
-
-while True:
-    time.sleep(1)
-    x, y = pyautogui.position()  # Get the current mouse position.
-    px = pyautogui.pixel(x, y)   # Get the pixel color at the mouse position.
-    print("px", px)
--- reactionTime.py
+++ reactionTime.py
@@ -1,28 +1,8 @@
 import pyautogui
-import time
-import keyboard  # Import the keyboard library
 
-# Wait for the user to press 's' before starting the loop
-print("Press 's' to start the script...")
-keyboard.wait('s')
+while True:
+    x, y = pyautogui.position()
+    rgb_red = pyautogui.pixel(x, y)[0]
 
-epoch = 0
-
-while epoch < 5:
-    x, y = pyautogui.position()  # Get the current mouse position.
-    red = pyautogui.pixel(x, y)[0]  # Get the red pixel value at the mouse position.
-
-    match red:
-        case 43:    # BLUE
-              print("Starting Round...")
-              time.sleep(1)
-              pyautogui.click()
-        # case 206:   # RED
-        #       continue
-        case 75:    # GREEN
-              pyautogui.click()
-              print("GREEN!")
-              epoch+=1
-
-
-print("Well Done!")
+    if rgb_red == 75 or rgb_red == 43:
+        pyautogui.click()
